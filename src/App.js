a189768eb4cb9909bfd913ae30f04e5ad9970c8f
iqbal <111554475+noobcoder6602@users.noreply.github.com>
2023-07-23 19:39:26 +0530

    added editor

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import JoditEditor from "jodit-react"
+import { useRef, useState } from "react"
 
 function App() {
+  const editor = useRef(null)
+  const [content, setContent] = useState("")
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      hellow
+      <JoditEditor
+        ref={editor}
+        value={content}
+        onChange={(newContent) => setContent(newContent)}
+      />
     </div>
   );
 }
