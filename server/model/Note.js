cd68e974e70bb0db662ce7bb415739fb3667bbef
iqbal <111554475+noobcoder6602@users.noreply.github.com>
2023-08-08 00:18:04 +0530

    added mongoose

---
+++ server/model/Note.js
@@ -0,0 +1,8 @@
+const { Schema, model } = require("mongoose")
+
+const Note = new Schema({
+  _id: String,
+  data: Object,
+})
+
+module.exports = model("Note", Note)
--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose')
 const io = require("socket.io")(3001, {
     cors: {
         origin: "http://localhost:3000",
@@ -5,12 +6,18 @@ const io = require("socket.io")(3001, {
     },
 })
 
+mongoose.connect('mongodb://localhost/thinkshare', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+
 io.on("connection", socket => {
     socket.on('get-document', documentId => {
         const data = ""
         socket.join(documentId)
         socket.emit('load-document', data)
-        
+
         socket.on('send-changes', delta => {
             console.log(delta)
             socket.broadcast.emit('recieve-changes', delta)
