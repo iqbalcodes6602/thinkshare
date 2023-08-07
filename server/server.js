47c203cb3241f7bf35e666a1edbf51d6322dd5d1
iqbal <111554475+noobcoder6602@users.noreply.github.com>
2023-08-08 00:31:53 +0530

    model added

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const Note = require('./model/Note');
 const io = require("socket.io")(3001, {
     cors: {
         origin: "http://localhost:3000",
@@ -6,23 +7,40 @@ const io = require("socket.io")(3001, {
     },
 })
 
-mongoose.connect('mongodb://localhost/thinkshare', {
+const connectionParams = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-})
+};
+try {
+    mongoose.connect('mongodb://127.0.0.1:27017/thinkshare', connectionParams);
+    console.log("Connected to database successfully");
+} catch (error) {
+    console.log(error);
+    console.log("Could not connect database!");
+}
 
+const defaultValue = ""
 
 io.on("connection", socket => {
-    socket.on('get-document', documentId => {
-        const data = ""
-        socket.join(documentId)
-        socket.emit('load-document', data)
+    socket.on("get-document", async noteId => {
+        const note = await findOrCreateNote(noteId)
+        socket.join(noteId)
+        socket.emit("load-document", note.data)
 
-        socket.on('send-changes', delta => {
-            console.log(delta)
-            socket.broadcast.emit('recieve-changes', delta)
+        socket.on("send-changes", delta => {
+            socket.broadcast.to(noteId).emit("receive-changes", delta)
+        })
+
+        socket.on("save-document", async data => {
+            await Note.findByIdAndUpdate(noteId, { data })
         })
     })
-    console.log("here")
 })
-console.log("running")
+
+async function findOrCreateNote(id) {
+    if (id == null) return
+
+    const note = await Note.findById(id)
+    if (note) return note
+    return await Note.create({ _id: id, data: defaultValue })
+}
