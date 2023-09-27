463ec2dabe2c6c5c2bba410d39362c6cb2194f46
iqbal <111554475+noobcoder6602@users.noreply.github.com>
2023-09-27 23:58:44 +0530

    changes from docu to sticky note

--- server/models/Document.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require("mongoose");
-
-const Sticky = new Schema({
-  _id: String,
-  data: Object,
-  pageId: String, // Add this field to the schema
-});
-
-module.exports = model("Sticky", Sticky);
